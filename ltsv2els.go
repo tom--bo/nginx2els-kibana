8e327ae35e51d50a0ab7d4faa90a46908f0d229e
tom--bo <otsuka.t.2013@gmail.com>
2016-09-29 22:29:53 +0900

    Make tidy by fmt

--- ltsv2els.go
+++ ltsv2els.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"fmt"
 	"net/http"
-    "bytes"
 	"os"
-    "fmt"
-    "strings"
-//    "strconv"
-    "encoding/json"
+	"strings"
+	//    "strconv"
+	"encoding/json"
 )
 
 type NginxLog struct {
@@ -32,8 +32,8 @@ type NginxLog struct {
 func main() {
 	var fp *os.File
 	var err error
-    //fmt.Println(os.Args[1])
-    //fmt.Println(os.Args[2])
+	//fmt.Println(os.Args[1])
+	//fmt.Println(os.Args[2])
 
 	if len(os.Args) < 2 {
 		fp = os.Stdin
@@ -52,52 +52,52 @@ func main() {
 		// fmt.Println(scanner.Text())
 		line := scanner.Text()
 		items := strings.Split(line, "\t")
-        logmap:=make(map[string]string)
-        checkEmptyValue:=map[string]string{
-            "time":"null",
-            "host":"null",
-            "forwardedfor":"null",
-            "req":"null",
-            "size":"null",
-            "reqtime":"0",
-            "runtime":"0",
-            "apptime":"0",
-        }
-		for _, item := range items {
-            val := strings.SplitN(item, ":", 2)
-            if fill, ok:=checkEmptyValue[val[0]];ok{
-                if val[1]=="-"{
-                    val[1]=fill
-                }
-            }
-            logmap[val[0]]=val[1]
+		logmap := make(map[string]string)
+		checkEmptyValue := map[string]string{
+			"time":         "null",
+			"host":         "null",
+			"forwardedfor": "null",
+			"req":          "null",
+			"size":         "null",
+			"reqtime":      "0",
+			"runtime":      "0",
+			"apptime":      "0",
 		}
-        log:=NginxLog{
-            Time    : logmap["time"],
-            Host    : logmap["host"],
-            Forwardedfor : logmap["forwardedfor"],
-            Req     : logmap["req"],
-            Method  : logmap["method"],
-            Uri     : logmap["uri"],
-            Status  : logmap["status"],
-            Size    : logmap["size"],
-            Referer : logmap["referer"],
-            UA      : logmap["ua"],
-            Reqtime : logmap["reqtime"],
-            Runtime : logmap["runtime"],
-            Apptime : logmap["apptime"],
-            Cache   : logmap["cache"],
-            Vhost   : logmap["vhost"],
-        }
+		for _, item := range items {
+			val := strings.SplitN(item, ":", 2)
+			if fill, ok := checkEmptyValue[val[0]]; ok {
+				if val[1] == "-" {
+					val[1] = fill
+				}
+			}
+			logmap[val[0]] = val[1]
+		}
+		log := NginxLog{
+			Time:         logmap["time"],
+			Host:         logmap["host"],
+			Forwardedfor: logmap["forwardedfor"],
+			Req:          logmap["req"],
+			Method:       logmap["method"],
+			Uri:          logmap["uri"],
+			Status:       logmap["status"],
+			Size:         logmap["size"],
+			Referer:      logmap["referer"],
+			UA:           logmap["ua"],
+			Reqtime:      logmap["reqtime"],
+			Runtime:      logmap["runtime"],
+			Apptime:      logmap["apptime"],
+			Cache:        logmap["cache"],
+			Vhost:        logmap["vhost"],
+		}
 		data, err := json.Marshal(log)
 		if err != nil {
 			fmt.Print("JSON marshaling failed: %s", err)
 		}
 
-        //fmt.Println(string(data))
-        postJson(os.Args[2], data)
+		//fmt.Println(string(data))
+		postJson(os.Args[2], data)
 
-        // time.Sleep(10 * time.Millisecond)
+		// time.Sleep(10 * time.Millisecond)
 		// b := new(bytes.Buffer)
 		// json.NewEncoder(b).Encode(log)
 		// _, err2 := http.Post(os.Args[2], "application/json; charset=utf-8", log)
@@ -112,26 +112,23 @@ func main() {
 }
 
 func postJson(url string, data []byte) {
-        req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
-        if err != nil {
-            fmt.Println(err)
-            panic(err)
-        }
-        // req.Header.Set("Content-Length", strconv.Itoa(len(data)))
-        // req.Header.Set("Content-Length", strconv.Itoa(300))
-        req.Header.Set("Content-Type", "application/json")
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	// req.Header.Set("Content-Length", strconv.Itoa(len(data)))
+	// req.Header.Set("Content-Length", strconv.Itoa(300))
+	req.Header.Set("Content-Type", "application/json")
 
-        client := http.Client{}
-        resp, err2 := client.Do(req)
-        if err2 != nil {
-            fmt.Println(err2)
-            panic(err2)
-        }
+	client := http.Client{}
+	resp, err2 := client.Do(req)
+	if err2 != nil {
+		fmt.Println(err2)
+		panic(err2)
+	}
 
-        // fmt.Println(resp)
-        // fmt.Println(resp.Body)
-        defer resp.Body.Close()
+	// fmt.Println(resp)
+	// fmt.Println(resp.Body)
+	defer resp.Body.Close()
 }
-
-
-
